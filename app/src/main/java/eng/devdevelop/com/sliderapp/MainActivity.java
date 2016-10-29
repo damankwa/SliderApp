56399c97aad57479d6ab74ed7298f8d23b3caabd
David <david_amankwa@yahoo.ca>
2016-10-29 15:56:43 -0400

    first commit

---
+++ app/src/main/java/eng/devdevelop/com/sliderapp/MainActivity.java
@@ -0,0 +1,77 @@
+package eng.devdevelop.com.sliderapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends FragmentActivity {
+    private int[] layouts;
+    private ViewPager viewpager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.slide1,
+                R.layout.slide2,
+                R.layout.slide3
+               };
+
+        //pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewpager = (ViewPager)findViewById(R.id.view_pager);
+        viewpager.setAdapter(myViewPagerAdapter);
+
+    }
+
+
+    private class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+}
